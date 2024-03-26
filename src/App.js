43ad58dd3216c33b07eb6b3c45381850ca1699a1
merryghasemi <mblueir@gmail.com>
2024-03-27 01:51:27 +0330

    base Structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 
 import './App.css';
+import Footer from './components/ui/Footer';
+import { Navbar } from './components/ui/Navbar';
 
 function App() {
   return (
-    <div className="App">
+    <div>
     
-        hello
+     <Navbar/>
+      <div className='wrapper'>
+        <h2> Wrapper </h2>
+      </div>
+
+     <Footer/>
     </div>
   );
 }
